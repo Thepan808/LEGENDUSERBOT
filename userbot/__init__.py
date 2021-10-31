719270fcb697f3f97df14a87fc5a89790a4400d2
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-31 18:29:38 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -125,29 +125,3 @@ SUDO_LIST = {}
 
 from userbot.helpers import *
 from userbot.cmdhelp import CmdHelp
-
-APP_ID = os.environ.get("APP_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN2 = os.environ.get("BOT_TOKEN2", None)
-BOT_TOKEN3 = os.environ.get("BOT_TOKEN3", None)
-BOT_TOKEN4 = os.environ.get("BOT_TOKEN4", None)
-BOT_TOKEN5 = os.environ.get("BOT_TOKEN5", None)
-BOT_TOKEN6 = os.environ.get("BOT_TOKEN6", None)
-BOT_TOKEN7 = os.environ.get("BOT_TOKEN7", None)
-BOT_TOKEN8 = os.environ.get("BOT_TOKEN8", None)
-BOT_TOKEN9 = os.environ.get("BOT_TOKEN9", None)
-BOT_TOKEN10 = os.environ.get("BOT_TOKEN10", None)
-
-
-
-bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
-bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
-bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
-bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
-bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
-bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
-bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
-bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
-bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10) 
-
-
