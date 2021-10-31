5d842ce083ba664314aaa472973583cc87a2a49b
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-31 18:38:37 +0530

    Update spam.py

--- userbot/spam.py
+++ userbot/spam.py
@@ -1,11 +1,17 @@
-import os
-import sys
-import time
-from telethon.sessions import StringSession
+
 from telethon import TelegramClient
-from userbot.Config import Config
-from var import Var
+from userbot import bot
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+
+try:
+  from userbot import bot 
+except:
+  pass
+
 
 APP_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
@@ -30,3 +36,16 @@ bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7
 bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
 bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
 bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10) 
+
+if __name__=="__main__":
+  bot.start()
+  bot.run_until_disconnected()
+  bot2.run_until_disconnected()
+  bot3.run_until_disconnected()
+  bot4.run_until_disconnected()
+  bot5.run_until_disconnected()
+  bot6.run_until_disconnected()
+  bot7.run_until_disconnected()
+  bot8.run_until_disconnected()
+  bot9.run_until_disconnected()
+  bot10.run_until_disconnected()
