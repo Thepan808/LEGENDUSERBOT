a341e1183eab509d73680fd9d56a6726e3fb68b4
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-31 19:06:19 +0530

    Update spam.py

--- userbot/spam.py
+++ userbot/spam.py
@@ -1,4 +1,3 @@
-
 from telethon import TelegramClient
 import os
 import asyncio
@@ -24,27 +23,13 @@ BOT_TOKEN8 = os.environ.get("BOT_TOKEN8", None)
 BOT_TOKEN9 = os.environ.get("BOT_TOKEN9", None)
 BOT_TOKEN10 = os.environ.get("BOT_TOKEN10", None)
 
+bot2 = TelegramClient(
+    session="Legend-Bot",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.BOT_TOKEN2)
 
 
-bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
-bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
-bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
-bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
-bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
-bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
-bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
-bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
-bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10) 
-
-if __name__=="__main__":
-  bot.start()
-  bot.run_until_disconnected()
-  bot2.run_until_disconnected()
-  bot3.run_until_disconnected()
-  bot4.run_until_disconnected()
-  bot5.run_until_disconnected()
-  bot6.run_until_disconnected()
-  bot7.run_until_disconnected()
-  bot8.run_until_disconnected()
-  bot9.run_until_disconnected()
-  bot10.run_until_disconnected()
